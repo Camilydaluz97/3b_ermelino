509d5c150c8bb6cdddef5263b2548ca5efdd73a7
Camilydaluz97 <camilydaluzbck@gmail.com>
2024-10-25 19:38:07 +0000

    criar função nomeada e anõnima

---
+++ funcoes/funcoes02.js
@@ -0,0 +1,21 @@
+
+// FUNÇÕES NOMEADAS
+function logar(usuario, senha) {
+    alert('Logado com a função nomeada!')
+    alert('Olá, ' + usuario)
+}
+
+logar("camily123", 12345)
+
+
+
+// FUNÇÕES ANÔNIMAS
+const conectar = function (usuario, senha) {
+    alert('Logado com a função nomeada!')
+    alert('Olá, ' + usuario)
+}
+
+
+
+//ARROW FUNCTION OU FUNÇÕES FLECHA
+
