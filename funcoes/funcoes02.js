09477d0d284cc49d5cfd25988f78eb368cbd6271
Camilydaluz97 <camilydaluzbck@gmail.com>
2024-10-31 19:33:36 +0000

    Finalizar funções arrow e criar setTimeout

--- funcoes/funcoes02.js
+++ funcoes/funcoes02.js
@@ -1,4 +1,3 @@
-
 // FUNÇÕES NOMEADAS
 function logar(usuario, senha) {
     alert('Logado com a função nomeada!')
@@ -11,11 +10,14 @@ logar("camily123", 12345)
 
 // FUNÇÕES ANÔNIMAS
 const conectar = function (usuario, senha) {
-    alert('Logado com a função nomeada!')
+    alert('Logado com a função anônima!')
     alert('Olá, ' + usuario)
 }
-
+conectar("Carla", "1789fds")
 
 
 //ARROW FUNCTION OU FUNÇÕES FLECHA
-
+const colorir = () => {
+    alert("Função flecha em ação!")
+}
+colorir() // execução da Arrow
